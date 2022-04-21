b07a01234bf1e4ed44a032b67a065cbc23dfcc03
goslyan <90595997+goslyan@users.noreply.github.com>
2022-04-21 14:39:29 +0300

    Add files via upload

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,7 +1,27 @@
-const calcForm = document.querySelector('.calc-block')
+const validate = () => {
+    const formName = document.querySelectorAll('[name="user_name"]')
+    const formEmail = document.querySelectorAll('[type=email]')
+    const formMsg = document.querySelectorAll('[placeholder="Ваше сообщение"]')
+    const formNum = document.querySelectorAll('[type=tel]')
 
-calcForm.addEventListener('input', () => {
-    if (e.target.type === 'number') {
-        e.target.value = e.target.value.replace(/\D/g, "")
-    }
-})
+    formName.forEach(formName =>
+        formName.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^а-яА-Я\-\s]/g, '') 
+    }))
+
+    formEmail.forEach(formEmail =>
+        formEmail.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^a-zA-z@\-\.\!\~\*\']/g, '') 
+    }))
+
+    formMsg.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/[^а-яА-Я\-\s]/g, '') 
+    })
+
+    formNum.forEach(formNum =>
+        formNum.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^0-9\(\)\-]/g, '') 
+    }))
+}
+
+validate()
